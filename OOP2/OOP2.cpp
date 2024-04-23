747c194563e2ad6ba3f2ac1fac685d1d03cb9fb6
lilalr <gurennut@gmail.com>
2024-04-23 19:58:02 +0700

    return panjang * lebar

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -20,18 +20,4 @@ public:
 		return panjang * lebar;
 	}
 
-	void display() {
-		cout << "Panjangnya = " << panjang << endl;
-		cout << "Lebarnya = " << lebar << endl;
-		cout << "Luasnya = " << hitungluas() << endl;
-	}
-};
-
-int main()
-{
-	bangunDatar pp; // deklarasi objek pp dari class bangunDatar
-	pp.input();
-	pp.display();
-
-	return 0;
-}
+	
