e72c6d0b37a178c08e7e78a1b9daaaa582d19589
lilalr <gurennut@gmail.com>
2024-04-23 19:56:17 +0700

    membuat prosedur display

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -20,4 +20,10 @@ public:
 		return panjang * lebar;
 	}
 
-	
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << lebar << endl;
+		cout << "Luasnya = " << hitungluas() << endl;
+	}
+};
+
